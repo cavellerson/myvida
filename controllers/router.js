972fe3798f672063a5e4f2bdd64d532e44347ae4
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2020-12-24 09:48:59 -0500

    seed data works?  commit

--- controllers/router.js
+++ controllers/router.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const Posts = require('../models/seed.js')
+const Post = require('../models/posts.js')
 
-router.get('', (req, res) => {
-    res.render('index.ejs',
-        {
-            Posts: Posts,
+
+router.get('/seed', (req, res) => {
+    Post.create(
+        [
+            {
+                title: "Rigatoni Alla Vodka for lunch:)",
+                description: "",
+                img: "shorturl.at/abefC",
+                tag: "food"
+            },
+            {
+                title: "Froyo",
+                description: "",
+                img: "shorturl.at/swHY6",
+                tag: "food"
+            },
+            {
+                title: "Sushi is my fav",
+                description: "",
+                img: "shorturl.at/chBFO".
+                tag: "food"
+            }
+        ],
+        (error, posts) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(`${posts} has been added`);
+                res.redirect('/home')
+            }
         }
     )
 })
