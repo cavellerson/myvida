736301b4e1cb3671707027feef0ee85c80404bad
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2020-12-24 08:54:47 -0500

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const methodOverride = require('method-override')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const app = express()
 const db = mongoose.connection
 require('dotenv').config()
@@ -8,6 +8,15 @@ require('dotenv').config()
 const PORT = process.env.PORT || 3003
 
 const MONGODB_URI = process.env.MONGODB_URI
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(methodOverride('_method'))
+
+
+const controller = require('./controllers/router.js')
+app.use('/home', controller);
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
@@ -18,13 +27,8 @@ mongoose.connect(MONGODB_URI, {
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'))
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI))
 db.on('disconnected', () => console.log('mongo disconnected'))
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(methodOverride('_method'))
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+
+
 app.listen(PORT, () => {
   console.log('Listening on port: ', PORT)
 })
