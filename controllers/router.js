33f3b62d83c1293aab75eb233266340a8f757f64
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2020-12-24 09:55:58 -0500

    seed data works?  commit

--- controllers/router.js
+++ controllers/router.js
@@ -12,37 +12,37 @@ router.get('', (req, res) => {
     })
 })
 
-router.get('/seed', (req, res) => {
-    Post.create(
-        [
-            {
-                title: "Rigatoni Alla Vodka for lunch:)",
-                description: "",
-                img: "shorturl.at/abefC",
-                tag: "food"
-            },
-            {
-                title: "Froyo",
-                description: "",
-                img: "shorturl.at/swHY6",
-                tag: "food"
-            },
-            {
-                title: "Sushi is my fav",
-                description: "",
-                img: "shorturl.at/chBFO".
-                tag: "food"
-            }
-        ],
-        (error, posts) => {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log(`${posts} has been added`);
-                res.redirect('/weddit')
-            }
-        }
-    )
-})
+// router.get('/seed', (req, res) => {
+//     Post.create(
+//         [
+//             {
+//                 title: "Rigatoni Alla Vodka for lunch:)",
+//                 description: "",
+//                 img: "shorturl.at/abefC",
+//                 tag: "food"
+//             },
+//             {
+//                 title: "Froyo",
+//                 description: "",
+//                 img: "shorturl.at/swHY6",
+//                 tag: "food"
+//             },
+//             {
+//                 title: "Sushi is my fav",
+//                 description: "",
+//                 img: "shorturl.at/chBFO".
+//                 tag: "food"
+//             }
+//         ],
+//         (error, posts) => {
+//             if (error) {
+//                 console.log(error);
+//             } else {
+//                 console.log(`${posts} has been added`);
+//                 res.redirect('/weddit')
+//             }
+//         }
+//     )
+// })
 
 module.exports = router;
