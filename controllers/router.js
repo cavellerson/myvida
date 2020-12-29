ee4ea40d5c9b0f520bac2a013e266a8052a0be8d
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2020-12-29 12:20:52 -0500

    project update

--- controllers/router.js
+++ controllers/router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/posts.js')
+const Posts = require('../models/seed.js')
 
 
 
@@ -10,47 +11,23 @@ router.get('', (req, res) => {
 
 router.get('/seed', (req, res) => {
     Post.create(
-        [
-            {
-                title: "Rigatoni Alla Vodka for lunch:)",
-                description: "",
-                img: "shorturl.at/abefC",
-                tag: "food"
-            },
-            {
-                title: "Froyo",
-                description: "",
-                img: "shorturl.at/swHY6",
-                tag: "food"
-            },
-            {
-                title: "Sushi is my fav",
-                description: "",
-                img: "shorturl.at/chBFO".
-                tag: "food"
-            }
-        ],
+        Posts,
         (error, posts) => {
             if (error) {
                 console.log(error);
-            } else {
-                console.log(`${posts} has been added`);
-                res.redirect('/gtgv')
+            }
+            else {
+                console.log(`${Posts} have been created`);
+                res.redirect('')
             }
         }
     )
 })
 
 
-router.get('/foods', (req, res) => {
-    Post.find({tag: "food"}, (error, allFoods) => {
-        res.render('foods.ejs',
-            {
-                foodPosts: allFoods,
-            }
-        )
-    })
-})
+
+
+
 
 
 
