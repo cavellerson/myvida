2a7f8a90cd71d6fce0f4558f0983674b3db78517
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2020-12-29 14:39:01 -0500

    trying to add seed data

--- controllers/router.js
+++ controllers/router.js
@@ -9,7 +9,27 @@ router.get('', (req, res) => {
     res.render('index.ejs')
 })
 
+router.get('/foods', (req, res) => {
+    Post.find({}, (error, allPosts) => {
+        res.render('foods.ejs',
+            {
+                posts: allPosts
+            }
+        )
+    })
+})
 
+router.get('/seed', (req, res) => {
+    post.create(foodPosts, (error, posts) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            console.log(`${posts} has been added to the database`);
+            res.redirect('/gtgv')
+        }
+    })
+})
 
 
 
