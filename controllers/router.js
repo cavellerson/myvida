94ffc1837a86618d34e3c9e76fec30d736259b47
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2020-12-29 12:36:03 -0500

    project update

--- controllers/router.js
+++ controllers/router.js
@@ -8,9 +8,29 @@ const foodPosts = require('../models/seed.js')
 router.get('', (req, res) => {
     res.render('index.ejs',
         {
-            foodposts: foodPosts,
+            foodposts: [
+                {
+                    title: "Rigatoni Alla Vodka for lunch:)",
+                    description: "",
+                    img: "shorturl.at/abefC",
+                    tag: "food"
+                },
+                {
+                    title: "Froyo",
+                    description: "",
+                    img: "shorturl.at/swHY6",
+                    tag: "food"
+                },
+                {
+                    title: "Sushi is my fav",
+                    description: "",
+                    img: "shorturl.at/chBFO".
+                    tag: "food"
+                }
+            ]
+,
         }
-)
+    )
 })
 
 
