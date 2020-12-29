4d8b989cb51f10ad985a4e279233c851e6003525
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2020-12-29 11:49:39 -0500

    project update

--- controllers/router.js
+++ controllers/router.js
@@ -8,53 +8,52 @@ router.get('', (req, res) => {
     res.render('index.ejs')
 })
 
-// router.get('', (req, res) => {
-//     res.render('index.ejs')
-// })
-//
-// router.get('/foods', (req, res) => {
-//     Post.find({tag: "food"}, (error, allFoods) => {
-//         res.render('foods.ejs',
-//             {
-//                 foodPosts: allFoods,
-//             }
-//         )
-//     })
-// })
+router.get('/seed', (req, res) => {
+    Post.create(
+        [
+            {
+                title: "Rigatoni Alla Vodka for lunch:)",
+                description: "",
+                img: "shorturl.at/abefC",
+                tag: "food"
+            },
+            {
+                title: "Froyo",
+                description: "",
+                img: "shorturl.at/swHY6",
+                tag: "food"
+            },
+            {
+                title: "Sushi is my fav",
+                description: "",
+                img: "shorturl.at/chBFO".
+                tag: "food"
+            }
+        ],
+        (error, posts) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(`${posts} has been added`);
+                res.redirect('/gtgv')
+            }
+        }
+    )
+})
+
+
+router.get('/foods', (req, res) => {
+    Post.find({tag: "food"}, (error, allFoods) => {
+        res.render('foods.ejs',
+            {
+                foodPosts: allFoods,
+            }
+        )
+    })
+})
 
 
 
-// router.get('/seed', (req, res) => {
-//     Post.create(
-//         [
-//             {
-//                 title: "Rigatoni Alla Vodka for lunch:)",
-//                 description: "",
-//                 img: "shorturl.at/abefC",
-//                 tag: "food"
-//             },
-//             {
-//                 title: "Froyo",
-//                 description: "",
-//                 img: "shorturl.at/swHY6",
-//                 tag: "food"
-//             },
-//             {
-//                 title: "Sushi is my fav",
-//                 description: "",
-//                 img: "shorturl.at/chBFO".
-//                 tag: "food"
-//             }
-//         ],
-//         (error, posts) => {
-//             if (error) {
-//                 console.log(error);
-//             } else {
-//                 console.log(`${posts} has been added`);
-//                 res.redirect('/gtgv')
-//             }
-//         }
-//     )
-// })
+
 
 module.exports = router;
