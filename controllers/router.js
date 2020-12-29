7544f35037fea692e0bdc1137fbb9cf331117057
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2020-12-29 12:32:51 -0500

    project update

--- controllers/router.js
+++ controllers/router.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/posts.js')
-
+const foodPosts = require('../models/seed.js')
 
 
 
 router.get('', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs',
+        {
+            foodPosts: foodPosts,
+        }
+)
 })
 
 
