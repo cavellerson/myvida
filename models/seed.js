0eb3381bf784db52b3d87100cc3d10e2b11ab263
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2020-12-29 13:38:47 -0500

    project update

--- models/seed.js
+++ models/seed.js
@@ -2,19 +2,19 @@ module.exports = [
     {
         title: "Rigatoni Alla Vodka for lunch:)",
         description: "",
-        img: "shorturl.at/abefC",
+        img: "https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s750x750/130928539_208210437564376_4689982508744768889_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=104&_nc_ohc=LOvXtj8WjRcAX9glkn1&tp=1&oh=d2465c769c4a0b09e9613a085c04532d&oe=600FE064",
         tag: "food"
     },
     {
         title: "Froyo",
         description: "",
-        img: "shorturl.at/swHY6",
+        img: "https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e35/15802198_1269865589740925_8730015435690344448_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=105&_nc_ohc=1TBqTxf4rFcAX-uH-qL&tp=1&oh=8d605520a719f340fe8fdcbeb431f0ea&oe=600E1ABA",
         tag: "food"
     },
     {
         title: "Sushi is my fav",
         description: "",
-        img: "shorturl.at/chBFO",
+        img: "https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p750x750/12912848_1602231040100814_461923725_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=106&_nc_ohc=ixk9uVqQ6bkAX851REH&tp=1&oh=145fdb7efe0d30c11d4997f29709848a&oe=600F46AE",
         tag: "food"
     }
 ]
